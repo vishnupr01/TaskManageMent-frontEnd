73a4be41ad5519fccfefa4236b7294d7d0a6ed3b
Vishnu P R <vishnuvicki3$@gmail.com>
2024-10-26 01:10:58 +0530

    ok

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const Api = axios.create({
-  baseURL: 'https://taskmanagement.vkart.fun',
+  baseURL: 'https://taskmanagement.vkart.fun/',
   withCredentials: true,
 });
 
